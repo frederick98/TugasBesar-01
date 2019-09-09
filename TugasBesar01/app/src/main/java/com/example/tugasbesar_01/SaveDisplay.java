77156563d176cf2c9d08be7b405db7517e61785e
LanangGalih <galihlanang000@gmail.com>
2019-09-09 22:23:37 +0700

    tambah 1 kelas

---
+++ TugasBesar01/app/src/main/java/com/example/tugasbesar_01/SaveDisplay.java
@@ -0,0 +1,46 @@
+package com.example.tugasbesar_01;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SaveDisplay {
+
+    protected SharedPreferences sharedPref;
+    protected final static  String NAMA_SHARED_PREF="sp_nilai_display";
+    protected final static String KEY_INPUT="INPUT";
+    protected final static String KEY_SPINNER="SPINNER";
+
+
+    public SaveDisplay(Context context)
+    {
+        this.sharedPref=context.getSharedPreferences(NAMA_SHARED_PREF,0);
+    }
+
+    public void saveInput(String input)
+    {
+        SharedPreferences.Editor editor=this.sharedPref.edit();
+        editor.putString(KEY_INPUT,input);
+        editor.commit();
+    }
+
+    public String getInput()
+    {
+        return sharedPref.getString(KEY_INPUT,"");
+    }
+
+    public void saveSpinner(String spinner)
+    {
+        SharedPreferences.Editor editor=this.sharedPref.edit();
+        editor.putString(KEY_SPINNER,spinner);
+        editor.commit();
+    }
+
+    public String getSpinner()
+    {
+        return sharedPref.getString(KEY_SPINNER,"");
+    }
+
+
+
+    
+}
